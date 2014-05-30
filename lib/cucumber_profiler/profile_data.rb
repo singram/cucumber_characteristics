970d53374d87ad386e454137c583cab5cfc6ba67
Stuart Ingram <ingrams2@upmc.com>
2014-05-30 00:00:11 -0400

    Added sorting

--- lib/cucumber_profiler/profile_data.rb
+++ lib/cucumber_profiler/profile_data.rb
@@ -17,9 +17,10 @@ module CucumberProfiler
       @step_profiles = {}
       @runtime.steps.each do |s|
         step_name = s.status == :undefined ? s.name : s.step_match.step_definition.file_colon_line
-        @step_profiles[step_name] ||= {:profile => {}, :passed => 0, :failed => 0, :skipped => 0, :undefined => 0}
+        @step_profiles[step_name] ||= {:profile => {}, :total_count => 0, :passed => 0, :failed => 0, :skipped => 0, :undefined => 0}
         feature_location = s.file_colon_line
         @step_profiles[step_name][s.status] += 1
+        @step_profiles[step_name][:total_count] += 1
         @step_profiles[step_name][:profile][feature_location] ||= []
         if s.status != :undefined
           @step_profiles[step_name][:regexp] = s.step_match.step_definition.regexp_source
